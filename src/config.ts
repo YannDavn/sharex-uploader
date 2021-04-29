a484d6f008f81e7ff55b6f50e693de4922fb85ce
Yann Davin <yanndvn@gmail.com>
2021-04-29 11:21:58 +0200

    [FEAT] Suppression des fichiers anciens à chaque requête d'upload

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,10 @@ import { readFileSync } from "fs";
 import { join } from "path";
 
 const defaultConfig = {
-    url: "https://example.com",
-    port: 4000,
-    uploadDir: join(process.cwd(), "uploads"),
+  url: "https://example.com",
+  port: 4000,
+  uploadDir: join(process.cwd(), "uploads"),
+  maxAge: 1000 * 60 * 60 * 24 * 7,
 };
 
 export const config: typeof defaultConfig = {
